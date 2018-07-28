d4846918147a3bd4ab546eff1d7098b090d9bc66
Markus Stöbe <markus@stoebe.me>
2018-07-28 12:02:29 +0200

    Initial Commit

---
+++ SiriShortcuts-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SiriShortcuts-Demo
+//
+//  Created by Markus Stöbe on 28.07.18.
+//  Copyright © 2018 Markus Stöbe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
